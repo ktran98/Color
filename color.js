4a54dab1b752b2ab13eb09eed09af864ffc4c65f
ktran98 <peterkhanhtran@gmail.com>
2016-11-03 13:58:43 -0500

    Add files via upload
    beginning with brightness, red+blue+green

--- color.js
+++ color.js
@@ -1,15 +1,18 @@
 //Declare the Color Object with our new keyword below here.
-const color = newObject();
-  this.red = R;
-  this.blue = B;
-  this.greed = G;
+const Color = new Object();
+
+/*
+** rgb is an array that hold the color values.
+** first is red -> R, then green -> G, and blue -> B
+** Example
+*/
 
 Color.isColor = function(rgb){
   //each color must be between 0 and 255. Also can be equal.
 
   let redworks = (rgb[0] >= 0) && (rgb[0] <= 255);
-  let greenworks = (rgb[1] >= 0) && (rgb[0] <= 255);
-  let blueworks = (rgb[2] >= 0) && (rgb[0] <= 255);
+  let greenworks = (rgb[1] >= 0) && (rgb[1] <= 255)
+  let blueworks = (rgb[2] >= 0) && (rgb[2] <= 255)
 
   if(redworks && greenworks && blueworks){
     return true;
@@ -20,22 +23,26 @@ Color.isColor = function(rgb){
 };
 
 Color.redIntensity = function(rgb){
-
+  let redIntensity = rgb[0]/255*100
+  redIntensity = Math.round(redIntensity)
+    return redIntensity
 };
 
 //greenIntensity
-Color.greenIntrnsity = function(rgb){
-
-
+Color.greenIntensity = function(rgb){
+  let greenIntensity = rgb[1]/225*100
+  greenIntensity = Math.round(greenIntensity)
+    return greenIntensity
 };
 
 //blueIntensity
-Color.blueIntensity = function(rgb){
-
-
+Color.greenIntensity = function(rgb){
+  let blueIntensity = rgb[2]/225*100
+  blueIntensity = Math.round(blueIntensity)
+    return blueIntensity
 };
 
 //brightness
-
+var bright = 
 
 //complement
